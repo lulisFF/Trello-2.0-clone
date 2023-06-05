8196742ec1a1ee7638b3414c48705ec7a41efdc7
Vyacheslav Shcherbakov <you@example.com>
2023-06-05 10:44:04 +0600

    feat: create a header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Trello 2.0 AI Clone</h1>
-    </main>
-  )
+    return (
+        <main>
+            {/*Header*/}
+            <Header />
+            {/*Board*/}
+            <h1>Trello 2.0 AI Clone</h1>
+        </main>
+    )
 }
